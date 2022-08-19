49c447c97aa6836cf9b9474c713a37bbb7f06ab0
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro-2.local>
2022-08-19 15:56:07 -0500

    resolver problema return tarea 1.1.1

--- Actividades/Recursion/Tarea1_1_1.cpp
+++ Actividades/Recursion/Tarea1_1_1.cpp
@@ -8,10 +8,10 @@ int fibonacchi_recursivo(int n, int lastnumber =0  , int numberbefore= 1 ){
         int x = lastnumber;
         lastnumber = lastnumber+numberbefore;
         numberbefore = x;
-        fibonacchi_recursivo(n-1 , lastnumber , numberbefore );
+        return fibonacchi_recursivo(n-1 , lastnumber , numberbefore );
     }
     else {        
-        cout<<lastnumber<<endl;
+        return lastnumber;
 
     }
 
@@ -24,17 +24,17 @@ int fibonacchi_iterativo(int n){
         lastnumber = lastnumber +numberbefore;
         numberbefore = x;
     }
-    cout<<lastnumber<<endl;
+    return lastnumber;
 }
 
 // Bacterias 
 
 float bacteriarecursiva(int n , float bacterias = 1){
     if(n> 0){
-        bacteriarecursiva(n-1 ,bacterias*(1+3.78-2.34));
+        return bacteriarecursiva(n-1 ,bacterias*(1+3.78-2.34));
     }
     else{
-        cout<<truncf(bacterias)<<endl;
+        return truncf(bacterias);
     };
 }
 float bacteriaiterativa(int n){
@@ -44,17 +44,17 @@ float bacteriaiterativa(int n){
         
 
     }
-    cout<<truncf(bacterias)<<endl;
+    return truncf(bacterias);
 }
 
 // Interes compuesto
 
 float inversionRecursiva(int meses , float cantidad){
     if(meses>0){
-        inversionRecursiva(meses-1, cantidad-(cantidad*0.1875));
+        return inversionRecursiva(meses-1, cantidad-(cantidad*0.1875));
     }
     else{
-        cout<<cantidad<<endl;
+        return cantidad;
     };
 }
 
@@ -62,54 +62,54 @@ float inversionIterativa(int meses , float cantidad){
     for(int i = 0 ;i<meses ;i++){
         cantidad=cantidad-(cantidad*0.1875);
     };
-    cout<<cantidad<<endl;
+    return cantidad;
 };
 
 // potencia
 
 int  potenciaRecursiva(int x , int y){
    if(y>1){
-       potenciaRecursiva(x*x, y-1);
+       return potenciaRecursiva(x*x, y-1);
    } 
    else{
-       cout<<x<<endl;
+       return x;
    };
 }
 int potenciaIterativa(int x , int y){
     for(int i = 0;i<=1;i++){
         x = x*x;
     }
-    cout<<x<<endl;
+    return x;
 }
 
 int main(){
     // llamar fibonachi con 10 iteraciones
     cout<<"fibonachi recursivo: ";
-    fibonacchi_recursivo(10);
+    cout<<fibonacchi_recursivo(10);
     cout<<endl;
     cout<<"fibonachi iterativo: ";
-    fibonacchi_iterativo(10);
+    cout<<fibonacchi_iterativo(10);
     cout<<endl;
     // llamar funciones bacterias con 10 dias
     cout<<"bacteria recursiva: ";
-    bacteriarecursiva(10);
+    cout<<bacteriarecursiva(10);
     cout<<endl;
     cout<<"bacteria iterativa: ";
-    bacteriaiterativa(10);
+    cout<<bacteriaiterativa(10);
     cout<<endl;
     // llamar interes compuesto con 10 meses con 10000 pesos 
     cout<<"inversion recursiva: ";
-    inversionRecursiva(10 , 10000);
+   cout<< inversionRecursiva(10 , 10000);
     cout<<endl;
     cout<<"inversion iterativa: ";
-    inversionIterativa(10 , 10000);
+    cout<<inversionIterativa(10 , 10000);
     cout<<endl;
    // Potencia de 10^5
     cout<<"potencia recursiva: ";
-    potenciaRecursiva(10,3);
+    cout<<potenciaRecursiva(10,3);
     cout<<endl;
     cout<<"potencia Iterativa:";
-    potenciaIterativa(10,3);
+    cout<<potenciaIterativa(10,3);
     cout<<endl;
 }
 
