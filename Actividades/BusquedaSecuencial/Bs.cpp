ad14f6b5a83ac0162cd7ca1f80b5b32f209dcf90
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro-2.local>
2022-08-30 16:42:48 -0500

    Busqueda Secuencial y binaria

--- Actividades/BusquedaSecuencial/Bs.cpp
+++ Actividades/BusquedaSecuencial/Bs.cpp
@@ -1,29 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 using namespace std;
 
-bool sequencialSearch(vector<int> list, int n) {
-    bool variable = false;
+int sequencialSearch(vector<int> list, int n) {
     for(int i = 0; i < list.size(); i++) {
         if (n == list[i]) {
-            variable = true;
+            return i;
         }
+        else if(i>=list.size()){cout<<"no esta en la lista"<<endl;}
     }
-        if (variable == true) {
-        cout << "Si esta en la lista";
-        }
-        else {
-        cout << "No esta en la lista";
-        }
 }
+
+int busuqedaBinaria(vector<int> list, int x)
+{
+  int size = list.size();
+  int key;
+  int left =0; 
+  int right = size-1;
+  while (left<=right){
+    key = left + (right -left)/2;
+    if (list[key]==x ){
+      return key; 
+      break;
+    }
+    else if (x<list[key]){
+      right=key-1; 
+    }
+    else if (x>list[key]) {
+      left=key+1;
+    }
+    else{
+        cout<<"no esta en la lista"<<endl;
+        return -1;
+        break;
+    }
+  }
+}
+
+
 
 int main() {
     vector<int> list;
-    list.push_back(2);
-    cout<<list[0];
+    for(int i = 0 ;i<=10 ;i++){
+        list.push_back(rand()%100+1);
+    }
+    
+    cout<<"dime que numero quieres buscar"<<endl;
    int n;
    cin >> n;
-   sequencialSearch(list, n);
+   cout<<"busqueda secuencial: "<<sequencialSearch(list, n)<<endl;
+   cout<<"busqueda binaria: "<<busuqedaBinaria(list, n)<<endl;
    return 0;
 }
 
