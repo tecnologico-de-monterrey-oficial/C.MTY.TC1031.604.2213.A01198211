0af438a82ee1347eb03203b333d0272905a35ac2
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2022-11-15 20:12:53 -0600

    Stack

--- ADTs/04-Stack/Stacktest.cpp
+++ ADTs/04-Stack/Stacktest.cpp
@@ -10,8 +10,10 @@ int main() {
   stack.push(4);
   stack.push(5);
   stack.push(6);
+  stack.print();
   std::cout << stack.getTop() << std::endl;
   std::cout << stack.pop() << std::endl;
   std::cout << stack.getTop() << std::endl;
+  stack.print();
   return 0;
 }
